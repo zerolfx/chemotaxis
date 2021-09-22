969793b319a4b00dc22ca0cebcae57992eb7c346
zerolfx <zerolfx0@gmail.com>
2021-09-22 16:01:10 -0400

    Update vis.py

--- src/maps/vis.py
+++ src/maps/vis.py
@@ -9,12 +9,12 @@ with open(file, 'r') as f:
     img = Image.new('RGB', (n, n), (255, 255, 255))
     pixels = img.load()
     sx, sy, tx, ty = map(int, f.readline().split())
-    pixels[sx - 1, sy - 1] = (0, 0, 255)
-    pixels[tx - 1, ty - 1] = (0, 255, 0)
+    pixels[sy - 1, sx - 1] = (0, 0, 255)
+    pixels[ty - 1, tx - 1] = (0, 255, 0)
     try:
         while True:
             x, y = map(int, f.readline().split())
-            pixels[x - 1, y - 1] = (0, 0, 0)
+            pixels[y - 1, x - 1] = (0, 0, 0)
     except Exception:
         pass
     img.save(file + '.png')
